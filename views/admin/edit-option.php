a8d17485caf855723946b0585bcfc886b244fdad
Eric Eaglstun <postpostmodern@gmail.com>
2016-12-29 12:06:44 -0600

    use render function for building dropdowns

---
+++ views/admin/edit-option.php
@@ -0,0 +1,3 @@
+<option class="level-<?php echo esc_attr( $depth ); ?>" value="<?php echo esc_attr( $term->slug ); ?>" <?php selected( $term->slug, $selected ); ?>>
+<?php echo esc_html( $display_name ); ?>
+</option>
