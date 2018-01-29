79d78eb9209143503bba7f77a814fcb0c55e90b7
Eric Eaglstun <postpostmodern@gmail.com>
2018-01-29 15:38:01 -0700

    fix create fucntion for php 7.2

--- _plugin.php
+++ _plugin.php
@@ -1,18 +1,24 @@
 <?php
 /**
 *   Plugin Name:    Taxonomy Taxi
-*   Plugin URI:     http://wordpress.org/plugins/taxonomy-taxi/
+*   Plugin URI:     https://wordpress.org/plugins/taxonomy-taxi/
 *   Description:    Show custom taxonomies in /wp-admin/edit.php automatically
-*   Version:        1.0.2
+*   Version:        1.0.3
 *   Author:         postpostmodern, pinecone-dot-website
-*   Author URI:     http://rack.and.pinecone.website
-*   Photo Credit:   http://www.flickr.com/photos/photos_mweber/
-*   Photo URL:      http://www.flickr.com/photos/photos_mweber/540970484/
+*   Author URI:     https://rack.and.pinecone.website
+*   Photo Credit:   https://www.flickr.com/photos/photos_mweber/
+*   Photo URL:      https://www.flickr.com/photos/photos_mweber/540970484/
 *   Photo License:  Attribution-NonCommercial 2.0 Generic (CC BY-NC 2.0)
 *   License:        GPL-2.0+
-*   License URI:    http://www.gnu.org/licenses/gpl-2.0.txt
+*   License URI:    https://www.gnu.org/licenses/gpl-2.0.txt
 */
 
-register_activation_hook( __FILE__, create_function("", '$ver = "5.3"; if( version_compare(phpversion(), $ver, "<") ) die( "This plugin requires PHP version $ver or greater be installed." );') );
-
-require __DIR__.'/index.php';
+if (version_compare(phpversion(), '5.4', "<")) {
+    add_action('admin_notices', create_function("", 'function(){
+        echo "<div class=\"notice notice-success is-dismissible\">
+                <p>Taxonomy Taxi requires PHP 5.4 or greater</p>
+              </div>";
+    };'));
+} else {
+    require __DIR__.'/index.php';
+}
